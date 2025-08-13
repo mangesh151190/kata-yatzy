563382418f9919d3fb91127dc20e6f3f6e43fd44
Mangesh <mangeshpramod.pawar@wolterskluwer.com>
2025-08-13 11:35:46 +0530

    used getCounts as common reusable method

--- yatzy-exercise-mango/src/main/java/YatzyJava.java
+++ yatzy-exercise-mango/src/main/java/YatzyJava.java
@@ -83,26 +83,17 @@ public class YatzyJava {
     }
 
     public static int score_pair(int d1, int d2, int d3, int d4, int d5) {
-        int[] counts = new int[6];
-        counts[d1 - 1]++;
-        counts[d2 - 1]++;
-        counts[d3 - 1]++;
-        counts[d4 - 1]++;
-        counts[d5 - 1]++;
-        int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6 - at - 1] >= 2)
-                return (6 - at) * 2;
+        int[] counts = getCounts(d1, d2, d3, d4, d5);
+        for (int i = 5; i >= 0; i--) {
+            if (counts[i] >= 2) {
+                return (i + 1) * 2;
+            }
+        }
         return 0;
     }
 
     public static int two_pair(int d1, int d2, int d3, int d4, int d5) {
-        int[] counts = new int[6];
-        counts[d1 - 1]++;
-        counts[d2 - 1]++;
-        counts[d3 - 1]++;
-        counts[d4 - 1]++;
-        counts[d5 - 1]++;
+        int[] counts = getCounts(d1, d2, d3, d4, d5);
         int n = 0;
         int score = 0;
         for (int i = 0; i < 6; i += 1)
@@ -116,14 +107,17 @@ public class YatzyJava {
             return 0;
     }
 
+    private static int[] getCounts(int... dice) {
+        int[] counts = new int[6];
+        for (int die : dice) {
+            counts[die - 1]++;
+        }
+        return counts;
+    }
+
+
     public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1 - 1]++;
-        tallies[_2 - 1]++;
-        tallies[d3 - 1]++;
-        tallies[d4 - 1]++;
-        tallies[d5 - 1]++;
+        int[] tallies = getCounts(_1, _2, d3, d4, d5);
         for (int i = 0; i < 6; i++)
             if (tallies[i] >= 4)
                 return (i + 1) * 4;
@@ -131,13 +125,7 @@ public class YatzyJava {
     }
 
     public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
-        int[] t;
-        t = new int[6];
-        t[d1 - 1]++;
-        t[d2 - 1]++;
-        t[d3 - 1]++;
-        t[d4 - 1]++;
-        t[d5 - 1]++;
+        int[] t = getCounts(d1, d2, d3, d4, d5);
         for (int i = 0; i < 6; i++)
             if (t[i] >= 3)
                 return (i + 1) * 3;
@@ -145,13 +133,7 @@ public class YatzyJava {
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
+        int[] tallies = getCounts(d1, d2, d3, d4, d5);
         if (tallies[0] == 1 &&
                 tallies[1] == 1 &&
                 tallies[2] == 1 &&
@@ -162,13 +144,7 @@ public class YatzyJava {
     }
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
+        int[] tallies = getCounts(d1, d2, d3, d4, d5);
         if (tallies[1] == 1 &&
                 tallies[2] == 1 &&
                 tallies[3] == 1 &&
@@ -186,13 +162,7 @@ public class YatzyJava {
         boolean _3 = false;
         int _3_at = 0;
 
-
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
+        tallies = getCounts(d1, d2, d3, d4, d5);
 
         for (i = 0; i != 6; i += 1)
             if (tallies[i] == 2) {
